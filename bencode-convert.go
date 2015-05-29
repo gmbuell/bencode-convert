e0c9a115a43308bcf27d44f9fd533ad2573c88f4
Garret Buell <gmbuell@gmail.com>
2015-05-29 16:21:22 -0700

    Add URL support

--- bencode-convert.go
+++ bencode-convert.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
@@ -14,19 +17,40 @@ import (
 
 var (
 	format = kingpin.Flag("format", "Output format, either JSON or YAML (default).").Default("yaml").String()
-	file   = kingpin.Arg("file", "Input file to parse.").Required().String()
+	path   = kingpin.Arg("path", "Input file or url to parse.").Required().String()
 )
 
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
 
-	dat, err := os.Open(*file)
-	if err != nil {
+	var dat io.ReadCloser
+	// Try and parse path as a url
+	url, err := url.Parse(*path)
+	if err != nil || url.Scheme == "" {
 		log.WithFields(log.Fields{
 			"Error": err,
-			"File":  *file,
-		}).Fatal("Error opening file.")
+			"File":  *path,
+		}).Info("Could not parse as a url.")
+
+		// Open a local file
+		dat, err = os.Open(*path)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"Error": err,
+				"File":  *path,
+			}).Fatal("Error opening file.")
+		}
+	} else {
+		var client http.Client
+		resp, err := client.Get(*path)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"Error": err,
+				"URL":   url,
+			}).Fatal("Error getting url.")
+		}
+		dat = resp.Body
 	}
 
 	decoded, err := bencode.Decode(dat)
@@ -34,7 +58,7 @@ func main() {
 	if err != nil {
 		log.WithFields(log.Fields{
 			"Error": err,
-			"File":  *file,
+			"File":  *path,
 		}).Fatal("Could not decode.")
 	}
 
